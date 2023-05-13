42e1e600b40bb83fea95d1fca007f71b6ae17003
Chris <77562903+darksaber9999@users.noreply.github.com>
2023-05-13 14:50:05 -0600

    Finish exercise 3

--- exercises/e3.js
+++ exercises/e3.js
@@ -1,7 +1,7 @@
 /**
  * PROMISES ARE ALWAYS ASYNCHRONOUS
  * Please, make sure to read the "03-Always-asynchronous.md" file in exercises-info folder before you start!
-*/
+ */
 
 /**
  * @task
@@ -10,17 +10,16 @@
  * * Takes an argument of any type
  * * If the argument is a whole number
  * * * Returns a promise constructor that resolves with a number value passed as the argument
- * * else returns 0; 
+ * * else returns 0;
  * * getPromise(num) function must be exported
  * Example: export const getPromise(num) => return <Your code of promise>
-*/ 
-export const getPromise = () => {
+ */
+export const getPromise = (input) => {
   // Your code goes here...
-
+  const newPromise = new Promise((resolve, reject) => resolve(input));
+  if (input - Math.floor(input) == 0) return newPromise;
+  return 0;
 };
-
-
-
 
 /**
  * @task
@@ -32,13 +31,14 @@ export const getPromise = () => {
  * * Synchronously adds 8 to the sum variable
  * * Returns the sum value
  * * if you have solved it successfully, the updateSumValue() function will return the value of 10;
-*/
-export const  updateSumValue = () => {
+ */
+export const updateSumValue = () => {
   // Your code goes here...
-  
-}
-
-
+  let sum = 2;
+  getPromise(120).then((data) => (sum += data));
+  sum += 8;
+  return sum;
+};
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-3"
